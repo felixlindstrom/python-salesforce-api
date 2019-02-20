a27425f6bc467f89c69f4d309414381d236158d5
felix.lindstrom <felix.lindstrom@bambora.com>
2019-02-20 12:00:19 +0100

    Fixed broken login method

--- salesforce_api/login.py
+++ salesforce_api/login.py
@@ -73,7 +73,7 @@ def oauth2(instance_url: str, client_id: str, client_secret: str, username: str,
     return plain_access_token(response_json['instance_url'], access_token=response_json['access_token'], session=session)
 
 
-def soap(instance_url: str, username: str, password: str, password_and_security_token: str, security_token: str, session: requests.Session = None) -> core.Connection:
+def soap(instance_url: str, username: str, password: str, security_token: str, password_and_security_token: str = None, session: requests.Session = None) -> core.Connection:
     session = misc_utils.get_session(session)
     instance_url = instance_url + '/services/Soap/c/' + const.API_VERSION
 
